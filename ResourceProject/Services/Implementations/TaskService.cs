ad18c66796c884893391341da7cc560442a5c173
debkantibarua1979 <129514042+debkantibarua1979@users.noreply.github.com>
2025-07-01 20:19:30 +0600

    Add TaskController and TaskService with CRUD operations for tasks

---
+++ ResourceProject/Services/Implementations/TaskService.cs
@@ -0,0 +1,38 @@
+using ResourceProject.Repositories.Interfaces;
+
+namespace ResourceProject.Services.Implementations;
+
+public class TaskService : ITaskService
+{
+    private readonly ITaskRepository _repository;
+
+    public TaskService(ITaskRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<List<Entities.Task>> GetAllAsync()
+    {
+        return await _repository.GetAllAsync();
+    }
+
+    public async Task<Entities.Task?> GetByIdAsync(Guid id)
+    {
+        return await _repository.GetByIdAsync(id);
+    }
+
+    public async Task<Entities.Task> CreateAsync(Entities.Task task)
+    {
+        return await _repository.CreateAsync(task);
+    }
+
+    public async Task<Entities.Task> UpdateAsync(Entities.Task task)
+    {
+        return await _repository.UpdateAsync(task);
+    }
+
+    public async Task<bool> DeleteAsync(Guid id)
+    {
+        return await _repository.DeleteAsync(id);
+    }
+}
---
+++ ResourceProject/Services/Interfaces/ITaskService.cs
@@ -0,0 +1,10 @@
+namespace ResourceProject.Services;
+
+public interface ITaskService
+{
+    Task<List<Entities.Task>> GetAllAsync();
+    Task<Entities.Task?> GetByIdAsync(Guid id);
+    Task<Entities.Task> CreateAsync(Entities.Task task);
+    Task<Entities.Task> UpdateAsync(Entities.Task task);
+    Task<bool> DeleteAsync(Guid id);
+}
